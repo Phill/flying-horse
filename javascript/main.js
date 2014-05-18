440770310c21c5fd2434f2a607cecfd65e915e5d
Yerani Charshafian <yerani@silverlakeranch.org>
2014-05-17 17:07:38 -0700

    move horse with spacebar

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,52 @@
  *
  */
 
+var pos = 100;
+var dir = "down";
+
 // Start the magic
 function init() {
   moveBackground(1);
-  moveHorseUpDown(450);
+// moveHorseUp(450);
+  moveHorseDown();
+  keyboard(); 
+}
+
+function keyboard(){
+  $("body").keydown(function (e){
+    keyCode = parseInt(e.keyCode);
+    if (keyCode == 32) { 
+      dir = "up";
+console.log("up");
+      moveHorseUp(pos); 
+    }
+  });
+
+  $("body").keyup(function (e){
+    keyCode = parseInt(e.keyCode);
+    if (keyCode == 32) { 
+      dir = "down";  
+console.log("down");
+    moveHorseDown(pos); 
+    }
+  });
+}
+
+function moveHorseUp() {
+  var speed  = (Math.floor((pos / 400) * 100)) + 5;
+  var top = 50;
+ if (dir == "down") return;
+  if (pos <= top){
+    return ;
+  } 
+
+   // Increment position
+  pos -= speed;
+
+  // Move horse to position
+  $('#horse').css({ top:pos });
+
+  setTimeout('moveHorseUp(' + pos + ')', 100);
 }
 
 // Moves the background
@@ -17,24 +59,21 @@ function moveBackground(i) {
 }
 
 // Bounces the horse
-function moveHorseUpDown(pos) {
+function moveHorseDown() {
   var speed  = (Math.floor((pos / 400) * 100)) + 5;
-  var top    = 5;
   var bottom = 343;
+  if (dir == "up") return;
+  if (pos >= bottom){
+    return ;
+  } 
 
-  if ( pos <= top )    dir = 'down';
-  if ( pos >= bottom ) dir = 'up';
-
-  if ( dir == 'up' )   posInc = -(speed);
-  if ( dir == 'down' ) posInc = speed;
-
-  // Increment position
-  pos += posInc;
+   // Increment position
+  pos += speed;
 
   // Move horse to position
   $('#horse').css({ top:pos });
 
-  setTimeout('moveHorseUpDown(' + pos + ')', 100);
+  setTimeout('moveHorseDown(' + pos + ')', 100);
 }
 
 // Where all the magic starts
